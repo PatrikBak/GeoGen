6e2e5b99e8cf57d06246e342813a54afccf74850
Patrik Bak <patrik.bak.x@gmail.com>
2019-11-23 23:31:32 +0100

    Added option for finding input files in subdirectories too

--- Source/GeoGen.ConsoleLauncher/BusinessLogic/Providers/AlgorithmInput/AlgorithmInputProvider.cs
+++ Source/GeoGen.ConsoleLauncher/BusinessLogic/Providers/AlgorithmInput/AlgorithmInputProvider.cs
@@ -60,7 +60,7 @@ namespace GeoGen.ConsoleLauncher
             LoggingManager.LogInfo($"Starting to search for input files in {_settings.InputFolderPath}");
 
             // Prepare the available input files
-            var inputFiles = Directory.EnumerateFiles(_settings.InputFolderPath, $"*.{_settings.FilesExtention}")
+            var inputFiles = Directory.EnumerateFiles(_settings.InputFolderPath, $"*.{_settings.FilesExtention}", SearchOption.AllDirectories)
                     // Find their file names
                     .Select(path => (path, fileName: Path.GetFileNameWithoutExtension(path)))
                     // Take only those that start with the requested pattern
