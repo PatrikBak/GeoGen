40abffcbd26e973dfbca52a2bc1dceea8487433b
Patrik Bak <patrik.bak.x@gmail.com>
2020-11-16 19:08:11 +0100

    New experiment ideas

--- Source/Launchers/GeoGen.InputGenerationLauncher/Program.cs
+++ Source/Launchers/GeoGen.InputGenerationLauncher/Program.cs
@@ -7,8 +7,10 @@ using GeoGen.TheoremFinder;
 using GeoGen.Utilities;
 using Ninject;
 using Ninject.Parameters;
+using Serilog;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using static GeoGen.Core.ConfigurationObjectType;
@@ -47,6 +49,9 @@ namespace GeoGen.InputGenerationLauncher
         /// </summary>
         private static void Main()
         {
+            // Setup simple logger
+            Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
+
             // Initialize the kernel
             _kernel = NinjectUtilities.CreateKernel()
                 // Add the constructor
@@ -80,7 +85,7 @@ namespace GeoGen.InputGenerationLauncher
 
             // If the folder exists, clear it
             if (Directory.Exists(resultsFolder))
-                Directory.EnumerateFiles(resultsFolder).ForEach(File.Delete);
+                Directory.Delete(resultsFolder, recursive: true);
 
             // Otherwise create it
             else
@@ -88,22 +93,32 @@ namespace GeoGen.InputGenerationLauncher
 
             #endregion
 
+            // Prepare the stopwatch
+            var stopwatch = Stopwatch.StartNew();
+
             // Prepare the counter of generated inputs
             var counter = 0;
 
             // Go through the all types of generated input files
             new[]
             {
-                TriangleTwoObjectsPlusThreeObjects(),
-                QuadrilateralAndTwoObjectsPlusTwoObjects(),
+                Triangle_TwoObjects_PlusThreeObjects(),
+                Triangle_FourObjects_OnlyFullySymmetric_PlusTwoObjects(),
+                Triangle_FourObjects_TwoLinesAndTwoPoints_OnlySymmetric_OnlyUsedLines_PlusTwoObjects(),
+                Triangle_FourObjects_ThreeLinesAndOnePoint_OnlySymmetric_OnlyUsedLines_PlusTwoObjects(),
+                Quadrilateral_TwoObjects_PlusTwoObjects()
             }
             // Merge the inputs
             .Flatten()
-            // For each create an input folder with the file
+            // Handle each generated file
             .ForEach(input =>
             {
                 // Count the input in
                 counter++;
+
+                // Log the count, but not too often
+                if (counter % 100 == 0)
+                    Log.Information("{count} files after {time} ms.", counter, stopwatch.ElapsedMilliseconds);
 
                 // Prepare the constructions as a single string
                 var constructionString = input.Constructions
@@ -144,18 +159,14 @@ namespace GeoGen.InputGenerationLauncher
             });
 
             // Log how many files have been created
-            Console.WriteLine($"Generated {counter} file(s)");
+            Log.Information("Generated {counter} file(s) after {time} ms.", counter, stopwatch.ElapsedMilliseconds);
         }
 
         #endregion
 
         #region Experiments
 
-        /// <summary>
-        /// An experiment where any triangle with two objects is extended by three more objects.
-        /// </summary>
-        /// <returns>The enumerable of inputs.</returns>
-        private static IEnumerable<ProblemGeneratorInput> TriangleTwoObjectsPlusThreeObjects()
+        private static IEnumerable<ProblemGeneratorInput> Triangle_TwoObjects_PlusThreeObjects()
         {
             // Create the loose objects
             var A = new LooseConfigurationObject(Point);
@@ -177,7 +188,7 @@ namespace GeoGen.InputGenerationLauncher
             var problemGeneratorInput = new ProblemGeneratorInput(configuration, _constructions, numberOfIterations: 2, maximalNumbersOfObjectsObjectsToAdd, SymmetryGenerationMode.GenerateBothSymmetricAndAsymmetric);
 
             // Prepare the generator settings
-            var settings = new ProblemGeneratorSettings(numberOfPictures: 5);
+            var settings = new ProblemGeneratorSettings(numberOfPictures: 2);
 
             // Return the generation enumerable by taking the generator 
             return _kernel.Get<IProblemGenerator>(new ConstructorArgument("settings", settings))
@@ -187,11 +198,11 @@ namespace GeoGen.InputGenerationLauncher
                 .generationOutputs
                 // Take the configuration
                 .Select(output => output.Configuration)
-                // That is on the last, i.e. second iteration
+                // That is on the last iteration
                 .Where(configuration => configuration.IterationIndex == 2)
                 // Every generated configuration makes an input file
                 .Select(configuration => new ProblemGeneratorInput(configuration, _constructions,
-                    // We will want 3 iterations
+                    // We will want plus 3 objects
                     numberOfIterations: 3,
                     // Set maximal numbers of objects to be added
                     new Dictionary<ConfigurationObjectType, int>
@@ -209,11 +220,7 @@ namespace GeoGen.InputGenerationLauncher
                     symmetryGenerationMode: SymmetryGenerationMode.GenerateOnlySymmetric));
         }
 
-        /// <summary>
-        /// An experiment where any quadrilateral with two objects is extended by two more objects.
-        /// </summary>
-        /// <returns>The enumerable of inputs.</returns>
-        private static IEnumerable<ProblemGeneratorInput> QuadrilateralAndTwoObjectsPlusTwoObjects()
+        private static IEnumerable<ProblemGeneratorInput> Quadrilateral_TwoObjects_PlusTwoObjects()
         {
             // Create the loose objects
             var A = new LooseConfigurationObject(Point);
@@ -236,7 +243,7 @@ namespace GeoGen.InputGenerationLauncher
             var problemGeneratorInput = new ProblemGeneratorInput(configuration, _constructions, numberOfIterations: 2, maximalNumbersOfObjectsObjectsToAdd, SymmetryGenerationMode.GenerateBothSymmetricAndAsymmetric);
 
             // Prepare the generator settings
-            var settings = new ProblemGeneratorSettings(numberOfPictures: 5);
+            var settings = new ProblemGeneratorSettings(numberOfPictures: 2);
 
             // Return the generation enumerable by taking the generator 
             return _kernel.Get<IProblemGenerator>(new ConstructorArgument("settings", settings))
@@ -246,8 +253,178 @@ namespace GeoGen.InputGenerationLauncher
                 .generationOutputs
                 // Take the configuration
                 .Select(output => output.Configuration)
-                // That is on the last, i.e. second iteration
+                // That is on the last iteration
                 .Where(configuration => configuration.IterationIndex == 2)
+                // Every generated configuration makes an input file
+                .Select(configuration => new ProblemGeneratorInput(configuration, _constructions,
+                    // We will want plus 2 objects
+                    numberOfIterations: 2,
+                    // Set maximal numbers of objects to be added
+                    new Dictionary<ConfigurationObjectType, int>
+                    {
+                        // Points and lines are not limited
+                        { Point, 2 },
+                        { Line, 2 },
+
+                        // We want at most 2 circles in total. More are not necessary, since circles
+                        // never appear as construction arguments, only in theorems itself, and at most
+                        // two of them (when we have two tangent circles)
+                        { Circle, 2 - configuration.ObjectMap.GetObjectsForKeys(Circle).Count() }
+                    },
+                    // We will want only symmetric results
+                    symmetryGenerationMode: SymmetryGenerationMode.GenerateOnlySymmetric));
+        }
+
+        private static IEnumerable<ProblemGeneratorInput> Triangle_FourObjects_OnlyFullySymmetric_PlusTwoObjects()
+        {
+            // Create the loose objects
+            var A = new LooseConfigurationObject(Point);
+            var B = new LooseConfigurationObject(Point);
+            var C = new LooseConfigurationObject(Point);
+
+            // Create the initial configuration
+            var configuration = Configuration.DeriveFromObjects(Triangle, A, B, C);
+
+            // Create the dictionary with the counts of objects to be added
+            var maximalNumbersOfObjectsObjectsToAdd = new Dictionary<ConfigurationObjectType, int>
+            {
+                { Point, 4 },
+                { Line, 4 },
+                { Circle, 2 }
+            };
+
+            // Prepare the generator input that takes only fully symmetric configurations
+            var problemGeneratorInput = new ProblemGeneratorInput(configuration, _constructions, numberOfIterations: 4, maximalNumbersOfObjectsObjectsToAdd, SymmetryGenerationMode.GenerateOnlyFullySymmetric);
+
+            // Prepare the generator settings
+            var settings = new ProblemGeneratorSettings(numberOfPictures: 2);
+
+            // Return the generation enumerable by taking the generator 
+            return _kernel.Get<IProblemGenerator>(new ConstructorArgument("settings", settings))
+                // Pass the input to it
+                .Generate(problemGeneratorInput)
+                // Unwrap the enumerable
+                .generationOutputs
+                // Take the configuration
+                .Select(output => output.Configuration)
+                // That is on the last iteration
+                .Where(configuration => configuration.IterationIndex == 3)
+                // Every generated configuration makes an input file
+                .Select(configuration => new ProblemGeneratorInput(configuration, _constructions,
+                    // We will want plus 2 objects
+                    numberOfIterations: 2,
+                    // Set maximal numbers of objects to be added
+                    new Dictionary<ConfigurationObjectType, int>
+                    {
+                        // Points and lines are not limited
+                        { Point, 2 },
+                        { Line, 2 },
+
+                        // We want at most 2 circles in total. More are not necessary, since circles
+                        // never appear as construction arguments, only in theorems itself, and at most
+                        // two of them (when we have two tangent circles)
+                        { Circle, 2 - configuration.ObjectMap.GetObjectsForKeys(Circle).Count() }
+                    },
+                    // We will want only symmetric results
+                    symmetryGenerationMode: SymmetryGenerationMode.GenerateOnlySymmetric));
+        }
+
+        private static IEnumerable<ProblemGeneratorInput> Triangle_FourObjects_TwoLinesAndTwoPoints_OnlySymmetric_OnlyUsedLines_PlusTwoObjects()
+        {
+            // Create the loose objects
+            var A = new LooseConfigurationObject(Point);
+            var B = new LooseConfigurationObject(Point);
+            var C = new LooseConfigurationObject(Point);
+
+            // Create the initial configuration
+            var configuration = Configuration.DeriveFromObjects(Triangle, A, B, C);
+
+            // Create the dictionary with the counts of objects to be added
+            var maximalNumbersOfObjectsObjectsToAdd = new Dictionary<ConfigurationObjectType, int>
+            {
+                { Point, 2 },
+                { Line, 2 },
+                { Circle, 0 }
+            };
+
+            // Prepare the generator input that takes only fully symmetric configurations
+            var problemGeneratorInput = new ProblemGeneratorInput(configuration, _constructions, numberOfIterations: 4, maximalNumbersOfObjectsObjectsToAdd, SymmetryGenerationMode.GenerateOnlySymmetric);
+
+            // Prepare the generator settings
+            var settings = new ProblemGeneratorSettings(numberOfPictures: 2);
+
+            // Return the generation enumerable by taking the generator 
+            return _kernel.Get<IProblemGenerator>(new ConstructorArgument("settings", settings))
+                // Pass the input to it
+                .Generate(problemGeneratorInput)
+                // Unwrap the enumerable
+                .generationOutputs
+                // Take the configuration
+                .Select(output => output.Configuration)
+                // That is on the last iteration
+                .Where(configuration => configuration.IterationIndex == 4)
+                // Exclude those where there is a hanging line
+                .Where(configuration => !configuration.ObjectMap.GetObjectsForKeys(Line).Any(line
+                    // We don't want a line that is not used in a construction    
+                    => configuration.ConstructedObjects.All(constructedObject => !constructedObject.PassedArguments.FlattenedList.Contains(line))))
+                // Every generated configuration makes an input file
+                .Select(configuration => new ProblemGeneratorInput(configuration, _constructions,
+                    // We will want 2 iterations
+                    numberOfIterations: 2,
+                    // Set maximal numbers of objects to be added
+                    new Dictionary<ConfigurationObjectType, int>
+                    {
+                        // Points and lines are not limited
+                        { Point, 2 },
+                        { Line, 2 },
+
+                        // We want at most 2 circles in total. More are not necessary, since circles
+                        // never appear as construction arguments, only in theorems itself, and at most
+                        // two of them (when we have two tangent circles)
+                        { Circle, 2 - configuration.ObjectMap.GetObjectsForKeys(Circle).Count() }
+                    },
+                    // We will want only symmetric results
+                    symmetryGenerationMode: SymmetryGenerationMode.GenerateOnlySymmetric));
+        }
+
+        private static IEnumerable<ProblemGeneratorInput> Triangle_FourObjects_ThreeLinesAndOnePoint_OnlySymmetric_OnlyUsedLines_PlusTwoObjects()
+        {
+            // Create the loose objects
+            var A = new LooseConfigurationObject(Point);
+            var B = new LooseConfigurationObject(Point);
+            var C = new LooseConfigurationObject(Point);
+
+            // Create the initial configuration
+            var configuration = Configuration.DeriveFromObjects(Triangle, A, B, C);
+
+            // Create the dictionary with the counts of objects to be added
+            var maximalNumbersOfObjectsObjectsToAdd = new Dictionary<ConfigurationObjectType, int>
+            {
+                { Point, 1 },
+                { Line, 3 },
+                { Circle, 0 }
+            };
+
+            // Prepare the generator input that takes only fully symmetric configurations
+            var problemGeneratorInput = new ProblemGeneratorInput(configuration, _constructions, numberOfIterations: 4, maximalNumbersOfObjectsObjectsToAdd, SymmetryGenerationMode.GenerateOnlySymmetric);
+
+            // Prepare the generator settings
+            var settings = new ProblemGeneratorSettings(numberOfPictures: 2);
+
+            // Return the generation enumerable by taking the generator 
+            return _kernel.Get<IProblemGenerator>(new ConstructorArgument("settings", settings))
+                // Pass the input to it
+                .Generate(problemGeneratorInput)
+                // Unwrap the enumerable
+                .generationOutputs
+                // Take the configuration
+                .Select(output => output.Configuration)
+                // That is on the last iteration
+                .Where(configuration => configuration.IterationIndex == 4)
+                // Exclude those where there is a hanging line
+                .Where(configuration => !configuration.ObjectMap.GetObjectsForKeys(Line).Any(line
+                    // We don't want a line that is not used in a construction    
+                    => configuration.ConstructedObjects.All(constructedObject => !constructedObject.PassedArguments.FlattenedList.Contains(line))))
                 // Every generated configuration makes an input file
                 .Select(configuration => new ProblemGeneratorInput(configuration, _constructions,
                     // We will want 2 iterations
