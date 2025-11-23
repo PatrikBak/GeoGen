982ae850f89575859426a02053a60a006fb51327
PatrikBak <patrik.bak.x@gmail.com>
2025-11-23 16:19:25 +0100

    Added Run scripts

---
+++ Source/Launchers/GeoGen.MainLauncher/Run.bat
@@ -0,0 +1,9 @@
+@echo off
+
+:: Run the application
+GeoGen.MainLauncher.exe
+
+:: Pause so the user can read the output
+echo.
+echo Press any key to exit...
+pause >nul
---
+++ Source/Launchers/GeoGen.MainLauncher/Run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Run the application
+./GeoGen.MainLauncher
+
+# Pause so the user can read the output
+echo
+read -p "Press Enter to exit..."
