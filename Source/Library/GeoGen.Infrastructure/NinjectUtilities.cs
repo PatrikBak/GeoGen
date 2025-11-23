a5c652c1e131cf7b0e3a7fe4e62a7e9d6747390b
PatrikBak <patrik.bak.x@gmail.com>
2025-11-23 15:12:55 +0100

    Making paths work with publish

--- Source/Library/GeoGen.Infrastructure/NinjectUtilities.cs
+++ Source/Library/GeoGen.Infrastructure/NinjectUtilities.cs
@@ -1,4 +1,5 @@
 ﻿using Ninject;
+using Ninject.Extensions.Factory;
 using Ninject.Planning.Bindings.Resolvers;
 
 namespace GeoGen.Infrastructure
@@ -23,6 +24,11 @@ namespace GeoGen.Infrastructure
 
             // Make sure NInject doesn't create instances that haven't been bound
             kernel.Components.Remove<IMissingBindingResolver, SelfBindingResolver>();
+
+            // Manually load factories, needed for with PublishSingleFile
+            // It is in a try-caatch because when running from a non-published state,
+            // it will throw an exception that it has already been loaded.
+            try { kernel.Load(new FuncModule()); } catch { }
 
             // Return it
             return kernel;
